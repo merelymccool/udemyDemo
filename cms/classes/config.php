5033f3fd4782d9a08dcafbb4df342f9bae6d3adf
Mia <51024591+mia@users.noreply.github.com>
2020-02-20 20:45:10 -0500

    Send email with mailtrap.io

---
+++ cms/classes/config.php
@@ -0,0 +1,9 @@
+<?php 
+
+
+class Config {
+    const SMTP_HOST = 'smtp.mailtrap.io';
+    const SMTP_PORT = 2525;
+    const SMTP_USER = 'a1b15ab09880f8';
+    const SMTP_PASSWORD = '3b935158ad250b';
+}
--- cms/forgot.php
+++ cms/forgot.php
@@ -2,6 +2,18 @@
 <?php  include "includes/header.php"; ?>
 <?php  include "./admin/includes/functions.php"; ?>
 
+<?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use phpmailer\phpmailer\PHPMailer;
+// use phpmailer\PHPMailer\SMTP;
+// use PHPMailer\PHPMailer\Exception;
+
+// Load Composer's autoloader
+require '../vendor/autoload.php';
+require 'classes/config.php';
+?>
+
 
 <?php 
 
@@ -20,6 +32,28 @@ if(ifItIsMethod('post')){
         mysqli_stmt_bind_param($stmt,"s",$email);
         mysqli_stmt_execute($stmt);
         mysqli_stmt_close($stmt);
+
+            // Instantiation and passing `true` enables exceptions
+            $mail = new PHPMailer(true);
+            $mail->isSMTP();                                           // Send using SMTP
+            $mail->Host       = Config::SMTP_HOST;                     // Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                  // Enable SMTP authentication
+            $mail->Username   = Config::SMTP_USER;                     // SMTP username
+            $mail->Password   = Config::SMTP_PASSWORD;                 // SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;        // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+            $mail->Port       = Config::SMTP_PORT;                     // TCP port to connect to
+
+            $mail->setFrom('admin@merelymccool.ca', 'Mia');
+            $mail->addAddress($email);     // Add a recipient
+            $mail->Subject = 'Here is the subject';
+            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+
+            if($mail->send()){
+                echo "It worked!";
+            } else {
+                echo "you dun fucked up";
+            }
+
         }
     }
 
